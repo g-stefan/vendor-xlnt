3a70080a652ef4208a0264f398393aa4bf83f8cf
Grigore Stefan <g_stefan@yahoo.com>
2021-07-05 12:31:39 +0300

    update

---
+++ build/msvc.install.sub.cmd
@@ -0,0 +1,26 @@
+@echo off
+rem Public domain
+rem http://unlicense.org/
+rem Created by Grigore Stefan <g_stefan@yahoo.com>
+
+rem --- make
+
+cmd.exe /C build\msvc.make.cmd
+if errorlevel 1 exit 1
+
+rem ---
+
+if not exist "%INSTALL_PATH_BIN%\" mkdir "%INSTALL_PATH_BIN%"
+xcopy /Y /S /E "temp\xlnt\bin\*.exe" "%INSTALL_PATH_BIN%\"
+xcopy /Y /S /E "temp\xlnt\bin\*.dll" "%INSTALL_PATH_BIN%\"
+
+rem --- dev
+
+if not exist "%INSTALL_PATH_DEV%\" mkdir "%INSTALL_PATH_DEV%"
+if not exist "%INSTALL_PATH_DEV%\bin" mkdir "%INSTALL_PATH_DEV%\bin"
+if not exist "%INSTALL_PATH_DEV%\include" mkdir "%INSTALL_PATH_DEV%\include"
+if not exist "%INSTALL_PATH_DEV%\lib" mkdir "%INSTALL_PATH_DEV%\lib"
+xcopy /Y /S /E "temp\xlnt\bin\*.exe" "%INSTALL_PATH_DEV%\bin\"
+xcopy /Y /S /E "temp\xlnt\bin\*.dll" "%INSTALL_PATH_DEV%\bin\"
+xcopy /Y /S /E "temp\xlnt\include\" "%INSTALL_PATH_DEV%\include\"
+copy /Y /B "temp\xlnt\lib\*.lib" "%INSTALL_PATH_DEV%\lib\*"
