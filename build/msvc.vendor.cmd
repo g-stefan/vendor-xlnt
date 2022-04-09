c5051006763315ec94a901ace896b1bb8614a8e8
Grigore Stefan <g_stefan@yahoo.com>
2022-04-09 21:09:59 +0300

    Update

--- build/msvc.vendor.cmd
+++ build/msvc.vendor.cmd
@@ -29,6 +29,6 @@ if not exist %VENDOR%.zip curl --insecure --location %WEB_LINK% --output %VENDOR
 del /F /Q %VENDOR%.zip
 del /F /Q pax_global_header 
 if exist %VENDOR%.7z del /F /Q %VENDOR%.7z
-7zr a -mx9 -mmt4 -r- -sse -w. -y -t7z %VENDOR%.7z %VENDOR%
+7z a -mx9 -mmt4 -r- -sse -w. -y -t7z %VENDOR%.7z %VENDOR%
 rmdir /Q /S %VENDOR%
 popd
